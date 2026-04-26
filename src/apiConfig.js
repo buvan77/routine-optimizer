c50898ff0d4ec1155dca59d328ef60137a7aab49
Buvanesh S R <buvansr00@gmail.com>
2026-04-26 12:20:41 +0530

    Delete src directory

--- src/apiConfig.js
+++
@@ -1,5 +0,0 @@
-const isLocal = window.location.hostname === "localhost";
-
-export const API_BASE_URL = isLocal 
-  ? "http://localhost:8080" 
-  : "https://2zbbtlct-8080.inc1.devtunnels.ms"; // <-- PASTE YOUR 8080 TUNNEL HERE
